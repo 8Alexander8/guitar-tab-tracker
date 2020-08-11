b86316ce23b0427db0035825b7a6af33f58d5022
unknown <alex131986@gmaul.com>
2020-08-11 22:10:02 +0300

    server init

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2020": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
